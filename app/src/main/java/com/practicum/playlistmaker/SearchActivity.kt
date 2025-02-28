ba3f7e5176852a1b03d9039beea1d66a3001741b
Polina-r <pfayzulaeva@gmail.com>
2025-02-28 14:17:57 +0300

    Commit 1.1.0 - Sprint 9 tasks 1-4

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,13 +1,86 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.internal.ViewUtils.hideKeyboard
 
 class SearchActivity : AppCompatActivity() {
+
+    var searchText: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         /*enableEdgeToEdge()*/
         setContentView(R.layout.activity_search)
+
+
+        // Toolbar "Поиск" и кнопка Назад
+        val toolbar = findViewById<Toolbar>(R.id.search_toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
+
+
+
+        // Поисковая строка
+        val searchEditText = findViewById<EditText>(R.id.searchEditText)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+
+        if (savedInstanceState != null) {
+            searchText = savedInstanceState.getString("search_text")
+            searchEditText.setText(searchText)
+        }
+
+        clearButton.setOnClickListener {
+            searchEditText.setText("")
+            hideKeyboard(searchEditText)
+        }
+
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                clearButton.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+                    // TODO заглушка
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // TODO заглушка
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // TODO заглушка
+            }
+        })
+
     }
+
+    // Функция убрать клавиатуру
+    private fun hideKeyboard(view: View) {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    // Функция сохранения текста
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("search_text", searchText)
+    }
+
+    // Функция восстановления текста
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchText = savedInstanceState.getString("search_text")
+    }
+
 }
--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,29 +1,128 @@
 package com.practicum.playlistmaker
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.SharedPreferences
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Switch
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.switchmaterial.SwitchMaterial
+import com.google.android.material.textview.MaterialTextView
 
 class SettingsActivity: AppCompatActivity() {
+
+    private lateinit var switchTheme: SwitchMaterial
+    private lateinit var sharedPreferences: SharedPreferences
+
+    @SuppressLint("WrongViewCast", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         /*enableEdgeToEdge()*/
         setContentView(R.layout.activity_settings)
 
-        /*val settingsReturnButton = findViewById<Button>(R.id.arrow_button)*/
+        this.sharedPreferences = getSharedPreferences("Settings", MODE_PRIVATE)
+
+        //Switch
+        this.switchTheme = findViewById(R.id.switchTheme)
+        switchTheme.isChecked = false
+
+        //Кнопка "Назад"
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        // Включаем кнопку "Назад"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
         toolbar.setNavigationOnClickListener {
             finish()
+        }
+
+        //Реализация переключения темы
+        val isDarkModeEnabled = sharedPreferences.getBoolean("darkMode", false)
+        switchTheme.isChecked = isDarkModeEnabled
+
+        switchTheme.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+
+            val editor = sharedPreferences.edit()
+            editor.putBoolean("darkMode", isChecked)
+            editor.apply()
+        }
+
+        //Кнопка "Поделиться приложением"
+            val shareButton = findViewById<MaterialTextView>(R.id.shareButton)
+
+        shareButton.setOnClickListener {
+            val message = "https://practicum.yandex.ru/profile/android-developer-plus/"
+            shareText(message)
+       }
+
+        //Кнопка "Написать в поддержку"
+        val supportButton = findViewById<MaterialTextView>(R.id.supportButton)
+        supportButton.setOnClickListener {
+            writeToSupport()
+        }
+
+        //Кнопка "Пользовательское соглашение"
+        val userAgreementButton = findViewById<MaterialTextView>(R.id.userAgreementButton)
+        userAgreementButton.setOnClickListener {
+            openAgreement()
+        }
+    }
+
+
+    //Функция "Поделиться приложением"
+    private fun shareText(message: String) {
+        //Создаём Intent
+        val shareIntent = Intent(Intent.ACTION_SEND)
+
+        // Тип данных
+        shareIntent.type = "text/plain"
+
+        shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+        startActivity(Intent.createChooser(shareIntent, getString(R.string.app_share)))
+    }
+
+    //Функция "Написать в поддержку"
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun writeToSupport() {
+        val supportIntent = Intent(Intent.ACTION_SENDTO)
+        // Тип данных
+        supportIntent.data = Uri.parse("mailto:")
+
+        supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("pfayzulaeva@gmail.com"))
+        supportIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject))
+        supportIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.email_body))
+
+        if (supportIntent.resolveActivity(packageManager) != null) {
+            startActivity(Intent.createChooser(supportIntent, "Отправить сообщение"))
+        } else {
+            Toast.makeText(this, getString(R.string.email_client_not_found), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    //Функция "Пользовательское соглашение"
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun openAgreement() {
+        val userAgreementUrl = "https://yandex.ru/legal/practicum_offer/"
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(userAgreementUrl))
+
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, getString(R.string.browser_not_found), Toast.LENGTH_SHORT).show()
         }
     }
 }
