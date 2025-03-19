3b4efaf3e938c724c6e94a2da6beabb36a1eb066
Polina <78693080+Polina-r@users.noreply.github.com>
2025-03-19 20:43:05 +0300

    Revert "PlayList Macker"

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,47 +1,16 @@
 package com.practicum.playlistmaker
 
-import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        /*enableEdgeToEdge()*/
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        val toolbar = findViewById<Toolbar>(R.id.main_toolbar)
-        setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(false) // Убираем кнопку назад, если не нужна
-
-        val searchButton = findViewById<Button>(R.id.search_button)
-        val libraryButton = findViewById<Button>(R.id.library_button)
-        val settingsButton = findViewById<Button>(R.id.settings_button)
-
-        searchButton.setOnClickListener {
-            val searchDisplayIntent = Intent(this, SearchActivity::class.java)
-            startActivity(searchDisplayIntent)
-        }
-        libraryButton.setOnClickListener {
-            val libraryDisplayIntent = Intent(this, LibraryActivity::class.java)
-            startActivity(libraryDisplayIntent)
-        }
-
-        settingsButton.setOnClickListener {
-            val settingsDisplayIntent = Intent(this, SettingsActivity::class.java)
-            startActivity(settingsDisplayIntent)
-        }
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
