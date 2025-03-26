aad2d426a388a7b086855456a33127512feb4a17
Polina-r <pfayzulaeva@gmail.com>
2025-03-26 17:32:25 +0300

    Commit 3.2.0 - Sprint 10 review fixes

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -64,14 +64,13 @@ class SearchActivity : AppCompatActivity() {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                searchText = s?.toString()
-                trackAdapter.filterList(searchText.orEmpty())
+                // TODO заглушка
             }
         })
         //Список треков RecyclerView
         recyclerView = findViewById(R.id.recyclerView)
 
-        trackList.add(Track("Sm Like Teen Spirit", "Nirvana", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"))
+        trackList.add(Track("Smells Like Teen Spirit", "Nirvana", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"))
         trackList.add(Track("Billie Jean", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"))
         trackList.add(Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"))
         trackList.add(Track("Whole Lotta Love", "Led Zeppelin", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"))
---
+++ app/src/main/java/com/practicum/playlistmaker/TrackViewHolder.kt
@@ -0,0 +1,45 @@
+package com.practicum.playlistmaker
+
+import android.content.Context
+import android.content.res.Resources
+import android.util.TypedValue
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+
+
+class TrackViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val trackName: TextView = itemView.findViewById(R.id.trackName)
+    val artistName: TextView = itemView.findViewById(R.id.artistName)
+    val trackTime: TextView = itemView.findViewById(R.id.trackTime)
+    val trackImage: ImageView = itemView.findViewById(R.id.trackImage)
+    val iconBetweenArtistAndTime: ImageView = itemView.findViewById(R.id.iconBetweenArtistAndTime)
+
+
+    fun bind(track: Track) {
+        trackName.text = track.trackName
+        artistName.text = track.artistName
+        trackTime.text = track.trackTime
+
+        val context = itemView.context
+        val cornerRadiusPx = dpToPx(2f, context)
+
+        Glide.with(context)
+            .load(track.artworkUrl100)
+            .placeholder(R.drawable.placeholder)
+            .centerCrop()
+            .transform(RoundedCorners(cornerRadiusPx))
+            .into(trackImage)
+    }
+
+    private fun dpToPx(dp: Float, context: Context): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp,
+            context.resources.displayMetrics
+        ).toInt()
+    }
+}
